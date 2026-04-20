018ec197fb7ad15ac2a67bce546c4fe1bdd49044
ISP530 <dcac8704@gmail.com>
2026-04-20 18:41:06 +0500

    Update Supabase connection details
    Updated Supabase URL and anon key for database connection.

--- db.js
+++ db.js
@@ -4,8 +4,8 @@
    EDIT THESE TWO LINES ONLY:
    ============================================ */
 
-const SUPABASE_URL = 'YOUR_SUPABASE_URL_HERE'
-const SUPABASE_ANON_KEY = 'YOUR_SUPABASE_ANON_KEY_HERE'
+const SUPABASE_URL = 'https://oaukxhrautnqcrvkprup.supabase.co'
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9hdWt4aHJhdXRucWNydmtwcnVwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYyMTc1OTgsImV4cCI6MjA5MTc5MzU5OH0.3QnJbuhjlkLX3zykmf6kJcC_U_pv2RJeJNlveD5tAFE'
 
 /* ============================================
    DO NOT EDIT BELOW THIS LINE
